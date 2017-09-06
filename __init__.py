9bc5f2b28c8ddeb9f21c1bf96a3ba6e20e1f12b2
FabioRosado <fabioglrosado@gmail.com>
2017-09-06 18:47:54 +0100

    Updated regex to accept both google and search

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@ import urllib.parse
 
 from opsdroid.matchers import match_regex
 
-@match_regex(r'^google (.*)')
+
+@match_regex(r'^(?:google|search) (.*)')
 async def hello(opsdroid, config, message):
     search_term = urllib.parse.quote_plus(message.regex.group(1))
-    google_url = config.get("engine-url", "https://www.google.co.uk/")
+    engine_url = config.get("engine-url", "https://www.google.co.uk/")
     query_arg = config.get("query-arg", "search?q=")
-    await message.respond("{}{}{}".format(google_url, query_arg, search_term))
+    await message.respond("Here's what I found {}{}{}".format(engine_url, query_arg, search_term))
